bd86d10d52b0c7e9f20f5b8d4becc16e8df841a8
Beers15 <alexanderbeers15@gmail.com>
2021-11-15 15:07:24 -0800

    Connect socket client to socket server

--- client3.js
+++ client3.js
@@ -1,8 +1,9 @@
 const io = require('socket.io-client');
-const socket = io('http://localhost:3000');
+//const socket = io('http://localhost:3000');
+const socket = io('https://chat-li-dev.herokuapp.com/');
 
 socket.on('connect', () => {
-  console.log('You are CONNECTED!')
+  console.log('You are CONNECTED!');
 });
 
 socket.emit('message', 'hello Kason'); // does not need to live in socket.on/'connect'
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+require('dotenv').config();
+
 const io = require('socket.io-client');
-const socket = io('http://localhost:3000');
+//const socket = io(process.env.SOCKET_SERVER_LOCAL);
+const socket = io(process.env.SOCKET_SERVER);
 
 socket.on('connect', () => {
-  console.log('You are CONNECTED!')
+  console.log('You are CONNECTED!');
 });
 
-socket.emit('message', 'hello Kason'); // does not need to live in socket.on/'connect'
+socket.emit('message', 'hello world');
 
 socket.on('Something', () => {
-  console.log('Yay! We did it!');
+  console.log('something event received from server');
 });
 
 socket.on('message', (payload) => {
-  console.log(payload);
+  console.log('RECEIVED: ', payload);
 });
 
 socket.on('disconnect', (reason) => {
