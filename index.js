b5eddc16a1e43dc42c7105b123b28c4693dace91
Christopher Burk <c.burk.tech@gmail.com>
2021-11-15 12:20:59 -0800

    renamed server to index, adds socket events and emits messages on event

---
+++ index.js
@@ -0,0 +1,22 @@
+const io = require('socket.io-client');
+const socket = io('http://localhost:3000');
+
+socket.on('connect', () => {
+  console.log('You are CONNECTED!')
+});
+
+socket.emit('message', 'hello Kason'); // does not need to live in socket.on/'connect'
+
+socket.on('message', (payload) => {
+  console.log(payload);
+});
+
+socket.on('disconnect', (reason) => {
+  console.log('Reason for disconnection: ', reason);
+});
+
+// TODO: Socket.io
+// setup message listener
+// rooms -> create, join, leave room
+
+// TODO: If reason is "transport close" -> send message with "sorry, our server closed"
