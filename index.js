605ae827ba31629399f08304d95cebea66a4a5a1
kason <kbraley9@gmail.com>
2021-11-16 14:12:25 -0700

    (refactor): Move code to src folder

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const io = require('socket.io-client');
 //const socket = io(process.env.SOCKET_SERVER_LOCAL);
 const socket = io(process.env.SOCKET_SERVER);
 
-require('./socket')(socket);
+require('./src/socket')(socket);
