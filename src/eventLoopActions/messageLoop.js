e89dc794858fe4479425f705c358cdf4b3081404
kason <kbraley9@gmail.com>
2021-11-16 15:47:53 -0700

    (feat): Add /listRoomUsers command
    Checks the room entered after the command and verify's it is a valid
    room name.

--- src/eventLoopActions/messageLoop.js
+++ src/eventLoopActions/messageLoop.js
@@ -1,4 +1,5 @@
 const rl = require('../utils/readLine');
+const rooms = require('../socket/rooms.json');
 
 const messageLoop = (socket, room) => {
   rl.setPrompt('>> ');
@@ -9,6 +10,20 @@ const messageLoop = (socket, room) => {
       console.log('Goodbye!');
       socket.emit('quit', {});
       process.exit();
+    } else if (input === '/listUsers') {
+      socket.emit('listUsers');
+    } else if (input.startsWith('/listRoomUsers')) {
+      let room = input.split(' ').pop().toLowerCase();
+
+      if (
+        !Object.values(rooms)
+          .map((value) => value.toLowerCase())
+          .includes(room)
+      ) {
+        console.log('[ERR] Invalid room name');
+      } else {
+        socket.emit('listRoomUsers', room);
+      }
     } else {
       console.log('You entered ' + input);
       let payload = {
