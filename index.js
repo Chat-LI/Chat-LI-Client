8f67ccbdd978fd0f695442afe5c57b82e2d6ab82
kason <kbraley9@gmail.com>
2021-11-18 16:04:30 -0700

    Remove comments

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const io = require('socket.io-client');
-//const socket = io(process.env.SOCKET_SERVER_LOCAL);
 const socket = io(process.env.SOCKET_SERVER);
 
 require('./src/socket')(socket);
